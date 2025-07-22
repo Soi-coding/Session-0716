31b4474bc906db84fe77dcdf1dc226f0384b949d
Soi-coding <2023112594@dgu.ac.kr>
2025-07-22 18:39:43 +0900

    src 파일 복구

---
+++ 13th_FE_API/src/velog-clone/src/page/Homepage.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { dummyData } from "../data/dummy_data";
+import PostCard from "../components/PostCard";
+
+function Homepage() {
+  return (
+      <div
+            style={{
+                    maxWidth: "1200px",
+                            margin: "0 auto",
+                                    padding: "20px",
+                                            display: "flex",
+                                                    flexDirection: "row",
+                                                    flexWrap: "wrap",
+                                                            gap: "20px",
+                                                                    justifyContent: "space-between",
+                                                                          }}
+                                                                              >
+                                                                                    {dummyData.map((post) => (
+                                                                                            <PostCard
+                                                                                                      key={post.postID}
+                                                                                                                title={post.title}
+                                                                                                                          content={post.content}
+                                                                                                                                    thumbnail={post.thumbnail}
+                                                                                                                                              createdAt={post.createdAt}
+                                                                                                                                                      />
+                                                                                                                                                            ))}
+                                                                                                                                                                </div>
+                                                                                                                                                                  );
+                                                                                                                                                                  }
+                                                                                                                                                                  
+                                                                                                                                                                  export default Homepage;
